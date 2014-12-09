1285d8122719a46f4e3eded959ebe8ec07443970
Christopher S. Case <chris.case@g33xnexus.com>
2014-12-09 10:35:47 -0600

    Finished the deck summary widgets.

--- client/deck/summary/summary.js
+++ client/deck/summary/summary.js
@@ -4,10 +4,32 @@
 // @module summary
 // ---------------------------------------------------------------------------------------------------------------------
 
-function DeckSummaryFactory()
+function DeckSummaryFactory(_)
 {
     function DeckSummaryController($scope)
     {
+        $scope.rating = ['empty', 'empty', 'empty', 'empty', 'empty'];
+
+        // Calculate rating
+        var rating = parseFloat($scope.deck.rating);
+        var wholeRating = Math.floor(rating);
+        var fractionalRating = rating - wholeRating;
+
+        // Fill the whole ratings
+        _.each(_.range(wholeRating), function(num, index)
+        {
+            $scope.rating[index] = 'full';
+        });
+
+        // Fill in the fractional rating
+        if(fractionalRating > .3 && fractionalRating <=.7)
+        {
+            $scope.rating[wholeRating] = 'half';
+        }
+        else if(fractionalRating > .7)
+        {
+            $scope.rating[wholeRating] = 'full';
+        } // end if
     } // end DeckSummaryController
 
     return {
@@ -23,6 +45,9 @@ function DeckSummaryFactory()
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-angular.module('card-crimes.directives').directive('deckSummary', [DeckSummaryFactory]);
+angular.module('card-crimes.directives').directive('deckSummary', [
+    'lodash',
+    DeckSummaryFactory
+]);
 
 // ---------------------------------------------------------------------------------------------------------------------
